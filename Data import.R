dc5e48915fc147a9e3b4436b3046448d3474b1ec
Beau Meche <beau_meche@college.harvard.edu>
2019-04-20 14:39:09 -0400

    tentative progress on panel 2's plot

--- Data import.R
+++ Data import.R
@@ -5,6 +5,7 @@ library(tidyverse)
 library(tigris)
 library(sf)
 library(ggmap)
+library(maps)
 
 miami <- read_csv(file = "miamidadecounty_fl.csv",
                   col_types = cols(
@@ -21,7 +22,8 @@ miami <- read_csv(file = "miamidadecounty_fl.csv",
                     year = col_double(),
                     month = col_double(),
                     yearmonth = col_double())) %>% 
-  filter(Latitude <= 27.0 & Latitude >= 24.0)
+  filter(Latitude <= 27.0 & Latitude >= 24.0) %>% 
+  filter(Longitude <= -79.0 & Longitude >= -82.0)
 
 #GPS stuff
 #NORTH: 27
@@ -42,9 +44,21 @@ map_2 <- st_as_sf(miami, coords = c("Longitude", "Latitude"),
                   crs = st_crs(shapes_305))
 
 
-ggplot(data = shapes_305) + geom_sf() +
-  geom_sf(data = map_2)
+ggplot(data = shapes_305) + geom_sf(color = "black", fill = "lightgreen") +
+  geom_sf(data = map_2, color = "black", alpha  = 0.4) + 
+  theme_bw() + 
+  theme(axis.text.x = element_blank())
+  
 
+# A look at the numbers:
+
+incidents <- miami %>% 
+  select(year, month) %>% 
+  group_by(year) %>% 
+  summarize(number = n()) %>% 
+  spread(year, number)
+  
+  
 
 
 
